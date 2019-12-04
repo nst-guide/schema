e9f7b44b623f62f018ebeda61f1206b42271d374
Kyle Barron <barronk@mit.edu>
2019-12-04 13:56:34 -0700

    Make schema_name case insensitive

--- nstschema/validate.py
+++ nstschema/validate.py
@@ -4,12 +4,18 @@ from jsonschema import validate as jsonvalidate
 from pkg_resources import resource_filename
 
 
-def validate(instance, table_name):
-    schema_path = get_schema_path(table_name)
+def validate(instance, schema_name):
+    """Validate JSON against schema
+
+    Args:
+        - instance (dict): JSON instance
+        - schema_name (str): name of schema to validate against
+    """
+    schema_path = get_schema_path(schema_name.lower())
     with open(schema_path) as f:
         schema = json.load(f)
     jsonvalidate(instance=instance, schema=schema)
 
 
-def get_schema_path(table_name):
-    return resource_filename('nstschema', f'schemas/{table_name}.json')
+def get_schema_path(schema_name):
+    return resource_filename('nstschema', f'schemas/{schema_name}.json')
